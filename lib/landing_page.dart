deb5dae1d1842ed56ffe5d37b2cd0c4b572e0bf0
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-07 16:17:38 +0800

    Add landing page to the app

---
+++ lib/landing_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'sign_in_up_page.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Spacer(),
+          Image.asset('assets/upatrol_logo.png'), // Logo
+          Text('UPATROL',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          Spacer(),
+          ElevatedButton(
+            onPressed: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => SignInUpPage())),
+            child: Text('Get Started'),
+          ),
+          // Add Quick Report Button here
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
