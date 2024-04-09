8c7744e8825b968015d5ab629867c95a710d09dc
Carl Jorenz Gimeno <csgimeno@up.edu.ph>
2024-04-09 21:47:59 +0800

    Redirect user to dashboard if user is logged in

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,61 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'components/template.dart';
+import 'landing_page.dart';
 import 'pages/dashboard.dart';
+import 'components/template.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+Future<void> main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure flutter bindings are initialized
   await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform);
-  await FirebaseAuth.instance.signInAnonymously();
+    options:
+        DefaultFirebaseOptions.currentPlatform, // Initialize Firebase
+  );
+  runApp(MyApp());
+}
 
-  runApp(const Template(child: Dashboard()));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UPatrol',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      if (FirebaseAuth.instance.currentUser != null) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (_) => Template(child: Dashboard())));
+      } else {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (_) => LandingPage()));
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/upatrol_logo.png'),
+      ),
+    );
+  }
 }
