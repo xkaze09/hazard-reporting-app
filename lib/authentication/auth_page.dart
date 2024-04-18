28a72af67d4406a47ad94b461fc434cb839c274a
CarlJorenzGimeno <csgimeno@up.edu.ph>
2024-04-18 15:21:32 +0800

    Update Firebase installation

--- lib/authentication/auth_page.dart
+++ lib/authentication/auth_page.dart
@@ -188,31 +188,29 @@ class _AuthPageState extends State<AuthPage> {
                   ],
                 ),
               ),
-                                    GestureDetector(
-                        onTap: () async {
-                          try {
-                            // Call the signInWithEmailAndPassword method and get UserCredential
-                            UserCredential authResult = await AuthService()
-                                .signInWithEmailAndPassword(
-                                    emailController.text,
-                                    passwordController.text);
+              GestureDetector(
+                onTap: () async {
+                  try {
+                    // Call the signInWithEmailAndPassword method and get UserCredential
+                    UserCredential authResult = await AuthService()
+                        .signInWithEmailAndPassword(
+                            emailController.text,
+                            passwordController.text);
 
-                            // Check if we have a User object
-                            if (authResult.user != null) {
-                              // Navigate to the role-based home page
-                              await AuthService().navigateToRoleBasedHomePage(
-                                  context, authResult.user!);
-                            }
-                          } on FirebaseAuthException catch (e) {
-                            String errorMessage = _getErrorMessage(e.code);
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text(errorMessage)),
-                            );
-                          }
-                        },
-
-                        
-                      ),
+                    // Check if we have a User object
+                    if (authResult.user != null) {
+                      // Navigate to the role-based home page
+                      await AuthService().navigateToRoleBasedHomePage(
+                          context, authResult.user!);
+                    }
+                  } on FirebaseAuthException catch (e) {
+                    String errorMessage = _getErrorMessage(e.code);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(errorMessage)),
+                    );
+                  }
+                },
+              ),
               if (isSignInSelected) // Sign In
                 Positioned(
                   top: height * .5 + 5,
@@ -256,7 +254,6 @@ class _AuthPageState extends State<AuthPage> {
                         ),
                       ),
                       SizedBox(height: 20),
-
                       Container(
                         width: width * 0.3,
                         child: ElevatedButton(
@@ -377,19 +374,19 @@ class _AuthPageState extends State<AuthPage> {
   }
 }
 
- String _getErrorMessage(String errorCode) {
-    switch (errorCode) {
-      case 'user-not-found':
-        return 'No user found for that email.';
-      case 'wrong-password':
-        return 'Wrong password provided for that user.';
-      case 'user-disabled':
-        return 'User has been disabled.';
-      case 'too-many-requests':
-        return 'Too many requests. Try again later.';
-      case 'operation-not-allowed':
-        return 'Signing in with Email and Password is not enabled.';
-      default:
-        return 'An unknown error occurred.';
-    }
+String _getErrorMessage(String errorCode) {
+  switch (errorCode) {
+    case 'user-not-found':
+      return 'No user found for that email.';
+    case 'wrong-password':
+      return 'Wrong password provided for that user.';
+    case 'user-disabled':
+      return 'User has been disabled.';
+    case 'too-many-requests':
+      return 'Too many requests. Try again later.';
+    case 'operation-not-allowed':
+      return 'Signing in with Email and Password is not enabled.';
+    default:
+      return 'An unknown error occurred.';
   }
+}
--- lib/data_types/globals.dart
+++ lib/data_types/globals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hazard_reporting_app/data_types/reports.dart';
+import 'package:hazard_reporting_app/data_types/utils.dart';
 
 final GlobalKey<ScaffoldMessengerState> rootScaffoldMessengerKey =
     GlobalKey<ScaffoldMessengerState>();
