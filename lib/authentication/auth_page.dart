0ae03a8dd4ccaef9d8a48f50bf6d06225f6a1595
Briana Jade Adricula <brianaadricula@gmail.com>
2024-04-18 19:05:15 +0800

    Update UI for auth page and landing page

--- lib/authentication/auth_page.dart
+++ lib/authentication/auth_page.dart
@@ -4,122 +4,348 @@ import 'package:hazard_reporting_app/data_types/globals.dart';
 import 'package:hazard_reporting_app/data_types/utils.dart';
 
 class AuthPage extends StatefulWidget {
-  const AuthPage({super.key});
+  final bool showSignUpFirst;
+
+  const AuthPage({super.key, this.showSignUpFirst = false});
 
   @override
   State<AuthPage> createState() => _AuthPageState();
 }
 
 class _AuthPageState extends State<AuthPage> {
-  final _signUpFormKey = GlobalKey<FormState>();
-  final _signInFormKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+  late Color myColor;
+  bool isSignInSelected = true;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController =
+      TextEditingController();
 
-  bool isObscure = true;
+  Widget _buildSocialButton(String logoPath, VoidCallback onTap) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        elevation: 2.0,
+      ),
+      onPressed: onTap,
+      child: Image.asset(logoPath, height: 30),
+    );
+  }
+
+  Widget _buildSocialButtonsRow() {
+    String text =
+        isSignInSelected ? 'or sign in with' : 'or sign up with';
+
+    return Column(
+      children: <Widget>[
+        Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+          ),
+        ),
+        SizedBox(height: 5),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildSocialButton(
+              'assets/images/facebook-logo.jpg',
+              () {
+                // Implement here if facebook is pressed
+              },
+            ),
+            SizedBox(width: 15),
+            _buildSocialButton(
+              'assets/images/google-logo.jpg',
+              () {
+                // Implement here if google is pressed
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        scaffoldMessengerKey: rootScaffoldMessengerKey,
-        home: Scaffold(
-          body: DefaultTabController(
-            length: 2,
-            child: Center(
-              child: SizedBox(
-                width: 500,
-                height: 600,
+    myColor = Theme.of(context).primaryColor;
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    if (widget.showSignUpFirst) {
+      isSignInSelected = false;
+    }
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Container(
+          height: height,
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.white,
                 child: Column(
                   children: [
-                    const TabBar(
-                      tabs: [
-                        Tab(
-                          child: Text('Sign In'),
-                        ),
-                        Tab(
-                          child: Text('Sign Up'),
-                        )
-                      ],
+                    Container(
+                      height: height * .3,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
                     ),
-                    SizedBox(
-                      width: 500,
-                      height: 500,
-                      child: TabBarView(children: [
-                        Form(
-                            key: _signInFormKey,
-                            child: Column(
-                              children: [
-                                TextFormField(
-                                    decoration: const InputDecoration(
-                                        labelText: 'Username',
-                                        hintText:
-                                            "Enter Email Address or Username"),
-                                    controller: _emailController,
-                                    validator: (value) {
-                                      if (value == null ||
-                                          value.isEmpty) {
-                                        return 'Email address required';
-                                      }
-                                      return null;
-                                    }),
-                                TextFormField(
-                                    obscureText: isObscure,
-                                    decoration: InputDecoration(
-                                        labelText: 'Password',
-                                        hintText: "Enter password",
-                                        suffixIcon: IconButton(
-                                          icon: Icon(isObscure
-                                              ? Icons.visibility
-                                              : Icons.visibility_off),
-                                          onPressed: () {
-                                            setState(() {
-                                              isObscure = !isObscure;
-                                            });
-                                          },
-                                        )),
-                                    controller: _passwordController,
-                                    validator: (value) {
-                                      if (value == null ||
-                                          value.isEmpty) {
-                                        return 'Password required';
-                                      }
-                                      return null;
-                                    }),
-                                TextButton(
-                                    onPressed: () async {
-                                      if (isSignedIn) {
-                                        showSnackBar(
-                                            "User is already signed in");
-                                      } else {
-                                        signInWithPassword(
-                                            context,
-                                            _emailController,
-                                            _passwordController);
-                                      }
-                                    },
-                                    child: const Text('Sign In')),
-                              ],
-                            )),
-                        Form(
-                            key: _signUpFormKey,
-                            child: Column(
-                              children: [
-                                TextFormField(validator: (value) {
-                                  if (value == null ||
-                                      value.isEmpty) {
-                                    return 'Email address required';
-                                  }
-                                  return null;
-                                })
-                              ],
-                            ))
-                      ]),
-                    )
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: Container(
+                        height: height * .7 - 30,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF146136),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(130),
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ),
+              Positioned(
+                top: height * .3 - (width * .45),
+                left: width * .1,
+                child: Container(
+                  height: width * .5,
+                  width: width * .8,
+                  child: Image(
+                    image:
+                        AssetImage("assets/images/UPatrol-logo.png"),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: height * .3 + (width * .2),
+                left: width * .2,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isSignInSelected = true;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text(
+                            "Sign In",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: isSignInSelected
+                                  ? Colors.white
+                                  : Colors.white.withOpacity(0.6),
+                            ),
+                          ),
+                          if (isSignInSelected)
+                            Container(
+                              margin: EdgeInsets.only(top: 5),
+                              width: 50,
+                              height: 2,
+                              color: Colors.white,
+                            ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isSignInSelected = false;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: isSignInSelected
+                                  ? Colors.white.withOpacity(0.6)
+                                  : Colors.white,
+                            ),
+                          ),
+                          if (!isSignInSelected)
+                            Container(
+                              margin: EdgeInsets.only(top: 5),
+                              width: 50,
+                              height: 2,
+                              color: Colors.white,
+                            ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              if (isSignInSelected) // Sign In
+                Positioned(
+                  top: height * .5 + 5,
+                  left: width * .1,
+                  child: Column(
+                    children: [
+                      Container(
+                        width: width * .8,
+                        padding: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon:
+                                Icon(Icons.email, color: Colors.grey),
+                            hintText: 'Email Address',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        width: width * .8,
+                        padding: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon:
+                                Icon(Icons.lock, color: Colors.grey),
+                            hintText: 'Password',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        width: width * 0.3,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Sign In',
+                              style: TextStyle(
+                                  fontSize: 17, color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF29AB84),
+                            textStyle: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(builder: (context) => ResetPasswordPage()),
+                          // );
+                        },
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.8)),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      _buildSocialButtonsRow(),
+                    ],
+                  ),
+                ),
+              if (!isSignInSelected) // Sign Up
+                Positioned(
+                  top: height * .5 + 10,
+                  left: width * .1,
+                  child: Column(
+                    children: [
+                      Container(
+                        width: width * .8,
+                        padding: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon:
+                                Icon(Icons.email, color: Colors.grey),
+                            hintText: 'Email Address',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Container(
+                        width: width * .8,
+                        padding: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon:
+                                Icon(Icons.lock, color: Colors.grey),
+                            hintText: 'Password',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Container(
+                        width: width * .8,
+                        padding: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextField(
+                          controller: confirmPasswordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon:
+                                Icon(Icons.lock, color: Colors.grey),
+                            hintText: 'Confirm Password',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 25),
+                      Container(
+                        width: width * 0.3,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Sign Up',
+                              style: TextStyle(
+                                  fontSize: 17, color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF29AB84),
+                            textStyle: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      _buildSocialButtonsRow(),
+                    ],
+                  ),
+                ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -3,32 +3,108 @@ import 'authentication/auth_page.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          //TODO Improve App Design
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Spacer(),
-            Image.asset('assets/upatrol_logo.png'), // Logo
-            const Text('UPATROL',
-                style: TextStyle(
-                    fontSize: 24, fontWeight: FontWeight.bold)),
-            const Spacer(),
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (_) => const AuthPage())),
-              child: const Text('Get Started'),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            bottom: -400,
+            child: CustomPaint(
+              painter: CurvePainter(),
             ),
-            // Add Quick Report Button here
-            const Spacer(),
-          ],
-        ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Transform.scale(
+                    scale: 1.17,
+                    child: Image.asset(
+                      'assets/images/UPatrol-logo.png',
+                      width: 600,
+                      height: 600,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AuthPage()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF29AB84),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(20.0),
+                            ),
+                            minimumSize: Size(200, 50),
+                          ),
+                          child: Text(
+                            'Get Started',
+                            style: TextStyle(
+                                color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            side: BorderSide(color: Colors.white),
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(20.0),
+                            ),
+                            minimumSize: Size(200, 50),
+                          ),
+                          child: Text(
+                            'Quick Report',
+                            style: TextStyle(
+                                color: Color(0xFF146136),
+                                fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Color(0xFF146136);
+    Path path = Path()
+      ..moveTo(0, size.height * 0.4)
+      ..quadraticBezierTo(size.width / 2, size.height / 2, size.width,
+          size.height * 0.4)
+      ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
