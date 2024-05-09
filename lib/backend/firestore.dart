97deeabdb735df6eb102c3dd091205f9651c9ee9
CarlJorenzGimeno <csgimeno@up.edu.ph>
2024-05-09 13:01:18 +0800

    Test

--- lib/backend/firestore.dart
+++ lib/backend/firestore.dart
@@ -64,7 +64,7 @@ class ImageStoreMethods {
           false, //isVerified,
           false,
           geoPoint, //location
-          address,
+          location,
           // (await usersCollection.doc(FirebaseAuth.instance.currentUser!.uid).get()) as DocumentReference<Object?>,   //reporter
           currentUserRef,
           Timestamp.now(), //timestamp
